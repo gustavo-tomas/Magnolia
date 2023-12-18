6a14d887a2ef990488b159ed98fb750db75f96d1
gustavo-tomas <guga.tomas10@gmail.com>
2023-12-18 19:26:29 -0300

    Adding performance counter

--- magnolia/src/core/application.cpp
+++ magnolia/src/core/application.cpp
@@ -36,8 +36,24 @@ namespace mag
 
     void Application::run()
     {
+        u64 last_time = 0, curr_time = SDL_GetPerformanceCounter(), frame_counter = 0;
+        f64 time_counter = 0.0, dt = 0.0;
+
         while (window.update())
         {
+            // Calculate dt
+            last_time = curr_time;
+            curr_time = SDL_GetPerformanceCounter();
+            dt = (curr_time - last_time) * 1000.0 / (SDL_GetPerformanceFrequency() * 1000.0);
+
+            frame_counter++;
+            time_counter += dt;
+            if (time_counter >= 1.0)
+            {
+                LOG_INFO("CPU: {0:.3f} ms/frame - {1} fps", 1000.0 / static_cast<f64>(frame_counter), frame_counter);
+                frame_counter = time_counter = 0;
+            }
+
             if (window.is_key_pressed(SDLK_ESCAPE)) window.set_capture_mouse(!window.is_mouse_captured());
             renderer.update();
         }
