a8298c2651cbeee0c9689be571f23fb6b72b77b3
gustavo-tomas <guga.tomas10@gmail.com>
2024-01-17 20:21:54 -0300

    Adding copy image and transfer methods

--- magnolia/src/renderer/command.hpp
+++ magnolia/src/renderer/command.hpp
@@ -16,6 +16,12 @@ namespace mag
             void begin_pass(const Pass& pass);
             void end_pass(const Pass& pass);
 
+            void copy_image_to_image(const vk::Image& src, const vk::Extent3D& src_extent, const vk::Image& dst,
+                                     const vk::Extent3D& dst_extent);
+
+            void transfer_layout(const vk::Image& image, const vk::ImageLayout curr_layout,
+                                 const vk::ImageLayout new_layout);
+
             const vk::CommandBuffer& get_handle() const { return this->command_buffer; }
 
         private:
