2e7a30bde93f4671b3367c260676536b906019a4
gustavo-tomas <guga.tomas10@gmail.com>
2024-01-17 14:15:59 -0300

    Adding vulkan image (no textures yet)

---
+++ magnolia/src/renderer/sampler.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+
+#include "core/types.hpp"
+
+namespace mag
+{
+    class Sampler
+    {
+        public:
+            void initialize(const vk::Filter min_mag_filter, const vk::SamplerAddressMode address_mode,
+                            const vk::SamplerMipmapMode mip_map_mode, const u32 mip_levels);
+            void shutdown();
+
+            const vk::Sampler& get_handle() const { return sampler; };
+
+        private:
+            vk::Sampler sampler;
+    };
+};  // namespace mag
