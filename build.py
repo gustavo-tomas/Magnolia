9c9dd60122e01f34b7c9427ec27ba329668b5f05
Iridium <moisxphillip@gmail.com>
2024-01-25 21:54:11 -0300

    beautified python commands :D

--- build.py
+++ build.py
@@ -6,14 +6,19 @@ import platform
 def build(system, configuration):
   # @TODO: temporary
   executable = f"premake5_{system}"
-  if configuration == "windows":
+  bar = "/"
+  if system == "windows":
     executable += ".exe"
-  assert os.system(f"libs/premake/{executable} gmake2 && cd build && make config={configuration} -j4") == 0
+    bar = "\\"
+  assert os.system(f"libs{bar}premake{bar}{executable} gmake2 && cd build && make config={configuration} -j4") == 0
   return
 
 # ----- Run -----
 def run(system, configuration):
-  assert os.system(f"build/{system}/magnolia_{configuration}") == 0
+  bar = "/"
+  if system == "windows":
+    bar = "\\"
+  assert os.system(f"build{bar}{system}{bar}magnolia_{configuration}") == 0
   return
 
 # ----- Clean -----
