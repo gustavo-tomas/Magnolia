d61419565203ae358f966312aa854916e95e9568
gustavo-tomas <guga.tomas10@gmail.com>
2024-02-10 09:07:21 -0300

    Adding spirv reflect lib

--- magnolia/src/renderer/shader.cpp
+++ magnolia/src/renderer/shader.cpp
@@ -4,6 +4,9 @@
 
 #include "core/logger.hpp"
 #include "renderer/context.hpp"
+#include "spirv_reflect.h"
+
+#define SPIRV_CHECK(result) ASSERT((result) == SPV_REFLECT_RESULT_SUCCESS, "Spirv check failed")
 
 namespace mag
 {
@@ -23,6 +26,28 @@ namespace mag
         file_stream.close();
 
         this->module = context.get_device().createShaderModule(vk::ShaderModuleCreateInfo({}, buffer));
+
+        // @TODO: testing
+        // Generate reflection data for a shader
+        {
+            SpvReflectShaderModule spv_module;
+            SpvReflectResult result = spvReflectCreateShaderModule(file_size, buffer.data(), &spv_module);
+            SPIRV_CHECK(result);
+
+            // Enumerate and extract shader's input variables
+            uint32_t var_count = 0;
+            result = spvReflectEnumerateInputVariables(&spv_module, &var_count, NULL);
+            SPIRV_CHECK(result);
+
+            SpvReflectInterfaceVariable** input_vars =
+                static_cast<SpvReflectInterfaceVariable**>(malloc(var_count * sizeof(SpvReflectInterfaceVariable*)));
+            result = spvReflectEnumerateInputVariables(&spv_module, &var_count, input_vars);
+            SPIRV_CHECK(result);
+
+            /* ... */
+
+            spvReflectDestroyShaderModule(&spv_module);
+        }
     }
 
     void Shader::shutdown()
