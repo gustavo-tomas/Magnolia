9e4b022fb1414efbf8cc554a86ec482984e3d721
gustavo-tomas <guga.tomas10@gmail.com>
2024-02-14 20:28:15 -0300

    Adding get near far method

--- magnolia/src/camera/camera.hpp
+++ magnolia/src/camera/camera.hpp
@@ -22,6 +22,7 @@ namespace mag
             const vec3& get_position() const { return position; };
             const vec3& get_rotation() const { return rotation; };
             const mat4& get_rotation_mat() const { return rotation_mat; };
+            vec2 get_near_far() const { return vec2(near, far); };
 
         private:
             void calculate_view();
