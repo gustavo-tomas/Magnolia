3197de6b9b1b6321e33ec7f9592180f349612c95
gustavo-tomas <guga.tomas10@gmail.com>
2024-02-17 08:59:30 -0300

    Changing default camera position

--- magnolia/src/renderer/renderer.cpp
+++ magnolia/src/renderer/renderer.cpp
@@ -24,7 +24,8 @@ namespace mag
         this->render_pass.initialize({context.get_surface_extent().width, context.get_surface_extent().height});
         LOG_SUCCESS("RenderPass initialized");
 
-        camera.initialize(vec3(1.5f, 0.7f, 1.5f), vec3(-20.0f, -45.0f, 0.0f), 60.0f, window.get_size(), 0.1f, 1000.0f);
+        camera.initialize(vec3(10.35f, 5.13f, 10.35f), vec3(-20.0f, -45.0f, 0.0f), 60.0f, window.get_size(), 0.1f,
+                          1000.0f);
         LOG_SUCCESS("Camera initialized");
 
         controller.initialize(&camera, &window);
