17c1d00043ee09bfdeec12d651d93258d241dfbe
gustavo-tomas <guga.tomas10@gmail.com>
2024-03-27 09:57:56 -0300

    Adding dt for resolution scaling controls

--- magnolia/src/renderer/renderer.cpp
+++ magnolia/src/renderer/renderer.cpp
@@ -53,10 +53,10 @@ namespace mag
 
         // @TODO: testing
         if (window->is_key_down(SDLK_UP))
-            render_pass.set_render_scale(render_pass.get_render_scale() + 0.01f);
+            render_pass.set_render_scale(render_pass.get_render_scale() + 0.15f * dt);
 
         else if (window->is_key_down(SDLK_DOWN))
-            render_pass.set_render_scale(render_pass.get_render_scale() - 0.01f);
+            render_pass.set_render_scale(render_pass.get_render_scale() - 0.15f * dt);
         // @TODO: testing
 
         // Draw calls
