08ebcfddf60d15716d1677eb5c6a5f2f44e8009c
gustavo-tomas <guga.tomas10@gmail.com>
2024-09-05 08:50:27 -0300

    Changing the editor camera initial position

--- magnolia/src/scene/scene.cpp
+++ magnolia/src/scene/scene.cpp
@@ -13,7 +13,7 @@ namespace mag
     Scene::Scene()
         : name("Untitled"),
           ecs(new ECS()),
-          camera(new Camera({-200.0f, 50.0f, 0.0f}, {0.0f, 90.0f, 0.0f}, 60.0f, 800.0f / 600.0f, 1.0f, 10000.0f)),
+          camera(new Camera({0.0f, 50.0f, 150.0f}, vec3(0.0f), 60.0f, 800.0f / 600.0f, 1.0f, 10000.0f)),
           camera_controller(new EditorCameraController(*camera))
     {
         auto& app = get_application();
