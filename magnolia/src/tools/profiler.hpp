b29fad7392a99eef373c6e340993b0d1c5b17b94
gustavo-tomas <guga.tomas10@gmail.com>
2024-09-29 16:25:24 -0300

    Adding frame average to profile results

--- magnolia/src/tools/profiler.hpp
+++ magnolia/src/tools/profiler.hpp
@@ -11,12 +11,41 @@ namespace mag
     struct ProfileResult
     {
             f64 duration;
+            f64 accumulated;
+            f64 average;
+            f64 frame_count;
+            f64 frame_start;
     };
 
     class ProfilerManager
     {
         public:
-            void update_profile_result(const str& name, const ProfileResult& result) { results[name] = result; }
+            void update_profile_result(const str& name, const f64 duration, const f64 time_interval_ms)
+            {
+                auto& app = get_application();
+                auto& window = app.get_window();
+
+                if (!results.contains(name))
+                {
+                    results[name] = {};
+                    results[name].frame_start = window.get_time();
+                }
+
+                results[name].duration = duration;
+                results[name].accumulated += duration;
+                results[name].frame_count++;
+
+                // Update average after N ms have passed
+                const f64 time_elapsed = window.get_time() - results[name].frame_start;
+                if (time_elapsed >= time_interval_ms)
+                {
+                    results[name].average = results[name].accumulated / results[name].frame_count;
+                    results[name].accumulated = 0;
+                    results[name].frame_count = 0;
+                    results[name].frame_start = window.get_time();
+                }
+            }
+
             void clear_results() { results.clear(); };
 
             const std::map<str, ProfileResult>& get_results() const { return results; };
@@ -29,13 +58,14 @@ namespace mag
 
         private:
             // Keep the results ordered
-            std::map<str, ProfileResult> results;
+            std::map<str, ProfileResult> results = {};
     };
 
     class ScopedProfiler
     {
         public:
-            ScopedProfiler(const str& name) : name(name)
+            ScopedProfiler(const str& name, const f64 time_interval_ms = 1000)
+                : name(name), time_interval_ms(time_interval_ms)
             {
                 auto& app = get_application();
                 auto& window = app.get_window();
@@ -49,20 +79,19 @@ namespace mag
                 auto& window = app.get_window();
 
                 const f64 end = window.get_time();
-                ProfileResult result = {end - start};
 
-                ProfilerManager::get().update_profile_result(name, result);
+                ProfilerManager::get().update_profile_result(name, end - start, time_interval_ms);
             }
 
         private:
             const str name;
-            f64 start;
+            f64 start, time_interval_ms;
     };
 };  // namespace mag
 
 // Don't use this macro twice in the same scope
 #if defined(MAG_PROFILE_ENABLED)
-    #define SCOPED_PROFILE(name) mag::ScopedProfiler scoped_profiler(name)
+    #define SCOPED_PROFILE(name, ...) mag::ScopedProfiler scoped_profiler(name, ##__VA_ARGS__)
 #else
-    #define SCOPED_PROFILE(name)
+    #define SCOPED_PROFILE(name, ...)
 #endif
