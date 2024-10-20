95aac13b1d4a3a90a5f77f2671c34a5aea057c93
gustavo-tomas <guga.tomas10@gmail.com>
2024-10-20 10:00:27 -0300

    Adding drag and drop for scripts

--- sprout_editor/src/panels/properties_panel.cpp
+++ sprout_editor/src/panels/properties_panel.cpp
@@ -1,12 +1,29 @@
 #include "panels/properties_panel.hpp"
 
+#include "core/application.hpp"
+#include "editor.hpp"
 #include "icon_font_cpp/IconsFontAwesome6.h"
+#include "imgui.h"
+#include "misc/cpp/imgui_stdlib.h"
 #include "resources/model.hpp"
 
 namespace sprout
 {
 #define MIN_VALUE -1'000'000'000
 #define MAX_VALUE +1'000'000'000
+
+    b8 check_file(const str &file_path)
+    {
+        auto &app = get_application();
+        auto &file_system = app.get_file_system();
+
+        if (file_system.exists(file_path) && !file_system.is_directory(file_path))
+        {
+            return file_system.get_file_extension(file_path) == ".cpp";
+        }
+
+        return false;
+    }
 
     void PropertiesPanel::render(const ImGuiWindowFlags window_flags, ECS &ecs, const u32 selected_entity_id)
     {
@@ -103,6 +120,43 @@ namespace sprout
 
                 component->camera.set_near_far({near, far});
                 component->camera.set_fov(fov);
+            }
+        }
+
+        if (auto component = ecs.get_component<ScriptComponent>(selected_entity_id))
+        {
+            if (ImGui::CollapsingHeader("Script", ImGuiTreeNodeFlags_DefaultOpen))
+            {
+                const ImGuiInputTextFlags flags =
+                    ImGuiInputTextFlags_CallbackHistory | ImGuiInputTextFlags_EnterReturnsTrue;
+
+                str file_path = component->file_path;
+
+                ImGui::Text("File");
+                if (ImGui::InputText("##File", &file_path, flags))
+                {
+                    // Check file validity
+                    if (check_file(file_path))
+                    {
+                        component->file_path = file_path;
+                    }
+                }
+
+                if (ImGui::BeginDragDropTarget())
+                {
+                    if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload(CONTENT_BROWSER_ITEM))
+                    {
+                        const c8 *file_path = static_cast<const c8 *>(payload->Data);
+
+                        // Check file validity
+                        if (check_file(file_path))
+                        {
+                            component->file_path = file_path;
+                        }
+                    }
+
+                    ImGui::EndDragDropTarget();
+                }
             }
         }
 
