47e8b74c408ba08eaea558e18d273a9c872a536a
gustavo-tomas <guga.tomas10@gmail.com>
2025-02-11 21:39:45 -0300

    Adding safety check for file dialogs

--- magnolia/src/platform/file_dialog.cpp
+++ magnolia/src/platform/file_dialog.cpp
@@ -7,6 +7,8 @@
 
 namespace mag
 {
+    b8 FileDialog::initialized = false;
+
     b8 FileDialog::initialize()
     {
 #if defined(MAG_DEBUG)
@@ -20,13 +22,16 @@ namespace mag
             return false;
         }
 
+        initialized = true;
         return true;
     }
 
-    void FileDialog::shutdown() {}
+    void FileDialog::shutdown() { initialized = false; }
 
     str FileDialog::open_file(const str& title, const std::vector<str>& filters)
     {
+        if (!initialized) return "";
+
         auto f = pfd::open_file(title, std::filesystem::current_path(), filters, pfd::opt::multiselect);
 
         const auto result = f.result();
@@ -40,6 +45,8 @@ namespace mag
 
     str FileDialog::save_file(const str& title, const str& file_name, const std::vector<str>& filters)
     {
+        if (!initialized) return "";
+
         auto f = pfd::save_file(title, std::filesystem::current_path() / file_name, filters);
 
         const auto result = f.result();
@@ -48,12 +55,16 @@ namespace mag
 
     void FileDialog::notify(const str& title, const str& message, const DialogIcon icon)
     {
+        if (!initialized) return;
+
         pfd::notify(title, message, static_cast<pfd::icon>(icon));
     }
 
     DialogButton FileDialog::message(const str& title, const str& message, const DialogChoice choice,
                                      const DialogIcon icon)
     {
+        if (!initialized) return DialogButton::Abort;
+
         auto m = pfd::message(title, message, static_cast<pfd::choice>(choice), static_cast<pfd::icon>(icon));
 
         return static_cast<DialogButton>(m.result());
@@ -61,6 +72,8 @@ namespace mag
 
     str FileDialog::select_folder(const str& title)
     {
+        if (!initialized) return "";
+
         auto dir = pfd::select_folder(title, std::filesystem::current_path());
 
         const auto result = dir.result();
