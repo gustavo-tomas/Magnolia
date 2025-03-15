25528017ef4c0a795c51235d6a3e4cbc33691b0f
gustavo-tomas <guga.tomas10@gmail.com>
2025-03-15 09:10:51 -0300

    Adding string cast to math headers

--- magnolia/src/math/generic.hpp
+++ magnolia/src/math/generic.hpp
@@ -4,7 +4,7 @@
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtx/matrix_decompose.hpp"
 #include "glm/gtx/quaternion.hpp"
+#include "glm/gtx/string_cast.hpp"
 
 // #include "glm/gtx/euler_angles.hpp"
-// #include "glm/gtx/string_cast.hpp"
 // #include "glm/gtx/transform.hpp"
