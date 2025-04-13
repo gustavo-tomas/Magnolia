bfa9d8e25a6fe2afc524329ef747077742f58ee4
gustavo-tomas <guga.tomas10@gmail.com>
2025-04-13 16:24:07 -0300

    Adding base game loop

--- test_game/src/application.cpp
+++ test_game/src/application.cpp
@@ -1,20 +1,88 @@
 #include "application.hpp"
 
+#include <core/application.hpp>
 #include <core/entry_point.hpp>
+#include <core/event.hpp>
+#include <project/project.hpp>
+#include <renderer/passes/scene_pass.hpp>
+#include <renderer/render_graph.hpp>
+#include <renderer/renderer.hpp>
+#include <scene/scene.hpp>
+#include <scene/scene_serializer.hpp>
 
 mag::Application *mag::create_application() { return new game::TestGame("test_game/config.json"); }
 
 namespace game
 {
-    TestGame::TestGame(const str &config_file_path) : Application(config_file_path) {}
+    TestGame::TestGame(const str &config_file_path) : Application(config_file_path)
+    {
+        mag::Project project;
+
+        const str project_file_path = "test_game/TestGame.proj.json";
+        if (!mag::project::load(project_file_path, project))
+        {
+            LOG_ERROR("Failed to load project: '{0}'", project_file_path);
+            return;
+        }
+
+        // Then load starting scene
+
+        scene = create_unique<mag::Scene>();
+
+        const str start_scene_file_path = project.get_asset_dir() / project.get_relative_start_scene_path();
+        if (!mag::scene::load(start_scene_file_path, *scene))
+        {
+            LOG_ERROR("Failed to load start scene: '{0}'", start_scene_file_path);
+            return;
+        }
+
+        mag::Application &app = mag::get_application();
+        mag::Window &window = app.get_window();
+
+        build_render_graph(window.get_size());
+
+        scene->on_start();
+    }
 
     TestGame::~TestGame() = default;
 
     void TestGame::on_update(const f32 dt)
     {
-        (void)dt;
-        LOG_INFO("Updating game. DT: {0}", dt);
+        scene->on_update(dt);
+
+        mag::Application &app = mag::get_application();
+        mag::Renderer &renderer = app.get_renderer();
+
+        renderer.on_update(*render_graph, *scene);
     }
 
-    void TestGame::on_event(const mag::Event &e) { (void)e; }
+    void TestGame::on_event(const mag::Event &e)
+    {
+        scene->on_event(e);
+
+        mag::dispatch_event<mag::WindowResizeEvent>(e,
+                                                    [this](const mag::WindowResizeEvent &e) {
+                                                        this->build_render_graph({e.width, e.height});
+                                                    });
+    }
+
+    void TestGame::build_render_graph(const uvec2 &size)
+    {
+        render_graph.reset(new mag::RenderGraph());
+
+        // @TODO: for now only one output attachment of each type is supported (one color and one depth maximum)
+        // @TODO: whatever change is made here has to be copied to the editor (or vice-versa) and this is not good :(
+
+        mag::DepthPrePass *depth_prepass = new mag::DepthPrePass(size);
+        mag::ScenePass *scene_pass = new mag::ScenePass(size);
+        mag::PostProcessingPass *post_pass = new mag::PostProcessingPass(size);
+
+        render_graph->set_output_attachment("OutputColor");
+
+        render_graph->add_pass(depth_prepass);
+        render_graph->add_pass(scene_pass);
+        render_graph->add_pass(post_pass);
+
+        render_graph->build();
+    }
 };  // namespace game
--- test_game/src/application.hpp
+++ test_game/src/application.hpp
@@ -2,8 +2,15 @@
 
 #include <magnolia.hpp>
 
+namespace mag
+{
+    class RenderGraph;
+};
+
 namespace game
 {
+    using namespace mag::math;
+
     class TestGame : public mag::Application
     {
         public:
@@ -12,5 +19,11 @@ namespace game
 
             virtual void on_update(const f32 dt) override;
             virtual void on_event(const mag::Event& e) override;
+
+        private:
+            void build_render_graph(const uvec2& size);
+
+            unique<mag::Scene> scene = nullptr;
+            unique<mag::RenderGraph> render_graph = nullptr;
     };
 };  // namespace game
