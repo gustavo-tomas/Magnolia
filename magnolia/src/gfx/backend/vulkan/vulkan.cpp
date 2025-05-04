ce211e384d526c666f397d6bdc3d8dd298064f02
gustavo-tomas <guga.tomas10@gmail.com>
2025-05-03 22:04:42 -0300

    Removing render pass (before dynamic rendering) and adding swapchain present mode

--- magnolia/src/gfx/backend/vulkan/vulkan.cpp
+++ magnolia/src/gfx/backend/vulkan/vulkan.cpp
@@ -72,7 +72,11 @@ namespace mag
                     // Swapchain
                     // -------------------------------------------------------------------------------------------------
                     vkb::SwapchainBuilder swapchain_builder{device};
-                    const auto swap_ret = swapchain_builder.set_old_swapchain(swapchain).build();
+                    const auto swap_ret = swapchain_builder.set_old_swapchain(swapchain)
+                                              .set_desired_present_mode(VK_PRESENT_MODE_MAILBOX_KHR)
+                                              .add_fallback_present_mode(VK_PRESENT_MODE_IMMEDIATE_KHR)
+                                              .add_fallback_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+                                              .build();
 
                     MAG_ASSERT(swap_ret, swap_ret.error().message() + " " + std::to_string(swap_ret.vk_result()));
 
@@ -95,50 +99,6 @@ namespace mag
                     MAG_ASSERT(present_queue_ret.has_value(), present_queue_ret.error().message());
 
                     present_queue = present_queue_ret.value();
-
-                    // RenderPass
-                    // -------------------------------------------------------------------------------------------------
-                    VkAttachmentDescription color_attachment = {};
-                    color_attachment.format = swapchain.image_format;
-                    color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
-                    color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-                    color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-                    color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-                    color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-                    color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-                    color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-
-                    VkAttachmentReference color_attachment_ref = {};
-                    color_attachment_ref.attachment = 0;
-                    color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-                    VkSubpassDescription subpass = {};
-                    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-                    subpass.colorAttachmentCount = 1;
-                    subpass.pColorAttachments = &color_attachment_ref;
-
-                    VkSubpassDependency dependency = {};
-                    dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-                    dependency.dstSubpass = 0;
-                    dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-                    dependency.srcAccessMask = 0;
-                    dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-                    dependency.dstAccessMask =
-                        VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
-
-                    VkRenderPassCreateInfo render_pass_info = {};
-                    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-                    render_pass_info.attachmentCount = 1;
-                    render_pass_info.pAttachments = &color_attachment;
-                    render_pass_info.subpassCount = 1;
-                    render_pass_info.pSubpasses = &subpass;
-                    render_pass_info.dependencyCount = 1;
-                    render_pass_info.pDependencies = &dependency;
-
-                    if (disp.createRenderPass(&render_pass_info, nullptr, &render_pass) != VK_SUCCESS)
-                    {
-                        MAG_ASSERT(false, "Failed to create renderpass");
-                    }
 
                     // Graphics Pipeline
                     // -------------------------------------------------------------------------------------------------
