689ff48c485b25964523fa6096f36cf34fd5361a
gustavo-tomas <guga.tomas10@gmail.com>
2025-05-06 14:00:57 -0300

    Adding frame data struct to hold frame relevant data

--- magnolia/src/gfx/gfx.cpp
+++ magnolia/src/gfx/gfx.cpp
@@ -9,6 +9,15 @@ namespace mag
         // @TODO: temporary
 #define MAX_FRAMES_IN_FLIGHT 3
 
+        struct FrameData
+        {
+                unique<ICommandPool> command_pool;
+                unique<ICommandBuffer> command_buffer;
+                unique<ISemaphore> available_semaphore;
+                unique<ISemaphore> finished_semaphore;
+                unique<IFence> in_flight_fence;
+        };
+
         struct GfxState
         {
                 unique<IDevice> device;
@@ -16,14 +25,7 @@ namespace mag
                 unique<IQueue> graphics_queue;
                 unique<IQueue> present_queue;
                 unique<IGraphicsPipeline> graphics_pipeline;
-                unique<ICommandPool> command_pool;
-                std::vector<unique<ICommandBuffer>> command_buffers;
-                std::vector<unique<IRenderPass>> render_passes;
-                std::vector<unique<IRenderingAttachment>> color_attachments;
-                std::vector<unique<ISemaphore>> available_semaphores;
-                std::vector<unique<ISemaphore>> finished_semaphore;
-                std::vector<unique<IFence>> in_flight_fences;
-                std::vector<IFence*> image_in_flight;
+                std::vector<FrameData> frames;
                 u32 current_frame = 0;
         };
 
@@ -53,84 +55,29 @@ namespace mag
             graphics_pipeline_desc.extent = state->swapchain->get_extent();
             state->graphics_pipeline = state->device->create_graphics_pipeline(graphics_pipeline_desc);
 
-            // Command Pool
+            // Command Pool, Command Buffers and Sync Objects
             // -------------------------------------------------------------------------------------------------
-            for (u32 i = 0; i < state->swapchain->get_image_count(); i++)
+            state->frames.resize(MAX_FRAMES_IN_FLIGHT);
+
+            for (u32 i = 0; i < state->frames.size(); i++)
             {
-                ICommandPoolDesc desc = {};
-                desc.queue_type = QueueType::Graphics;
-                state->command_pool = state->device->create_command_pool(desc);
-            }
+                ICommandPoolDesc command_pool_desc = {};
+                command_pool_desc.queue_type = QueueType::Graphics;
+                state->frames[i].command_pool = state->device->create_command_pool(command_pool_desc);
 
-            // Command Buffers
-            // -------------------------------------------------------------------------------------------------
-            for (u32 i = 0; i < state->swapchain->get_image_count(); i++)
-            {
-                ICommandBufferDesc desc = {};
-                desc.command_buffer_level = CommandBufferLevel::Primary;
-                desc.command_pool = state->command_pool.get();
-                state->command_buffers.push_back(state->device->create_command_buffer(desc));
-            }
+                ICommandBufferDesc command_buffer_desc = {};
+                command_buffer_desc.command_buffer_level = CommandBufferLevel::Primary;
+                command_buffer_desc.command_pool = state->frames[i].command_pool.get();
+                state->frames[i].command_buffer = state->device->create_command_buffer(command_buffer_desc);
 
-            // Render Passes
-            // -------------------------------------------------------------------------------------------------
-            state->color_attachments.resize(state->command_buffers.size());
-            state->render_passes.resize(state->command_buffers.size());
+                IFenceDesc fence_desc = {};
+                fence_desc.signaled = true;
 
-            for (u64 i = 0; i < state->command_buffers.size(); i++)
-            {
-                state->command_buffers[i]->begin_recording();
+                ISemaphoreDesc sem_desc = {};
 
-                IRenderingAttachmentDesc color_attachment_desc = {};
-                color_attachment_desc.type = RenderingAttachmentType::Color;
-                color_attachment_desc.clear_color = {0.4f, 0.6f, 0.8f, 1.0f};
-                color_attachment_desc.texture = state->swapchain->get_texture(i);
-                state->color_attachments[i] = state->device->create_render_attachment(color_attachment_desc);
-
-                IRenderPassDesc render_pass_desc = {};
-                render_pass_desc.extent = state->swapchain->get_extent();
-                render_pass_desc.color_attachments.push_back(state->color_attachments[i].get());
-                state->render_passes[i] = state->device->create_render_pass(render_pass_desc);
-
-                state->command_buffers[i]->pipeline_barrier(
-                    state->swapchain->get_texture(i), TextureLayout::ColorAttachment, AccessMask::None,
-                    AccessMask::ColorAttachmentWrite, PipelineStage::TopOfPipe, PipelineStage::ColorAttachmentOutput);
-
-                state->command_buffers[i]->set_viewport(state->swapchain->get_extent());
-                state->command_buffers[i]->set_scissor(state->swapchain->get_extent());
-
-                state->command_buffers[i]->begin_rendering(state->render_passes[i].get());
-
-                state->command_buffers[i]->bind_pipeline(state->graphics_pipeline.get());
-
-                state->command_buffers[i]->draw(3);
-
-                state->command_buffers[i]->end_rendering();
-
-                state->command_buffers[i]->pipeline_barrier(
-                    state->swapchain->get_texture(i), TextureLayout::Present, AccessMask::ColorAttachmentWrite,
-                    AccessMask::None, PipelineStage::ColorAttachmentOutput, PipelineStage::BottomOfPipe);
-
-                state->command_buffers[i]->end_recording();
-            }
-
-            // Sync Objects
-            // -------------------------------------------------------------------------------------------------
-            state->available_semaphores.resize(MAX_FRAMES_IN_FLIGHT);
-            state->finished_semaphore.resize(MAX_FRAMES_IN_FLIGHT);
-            state->in_flight_fences.resize(MAX_FRAMES_IN_FLIGHT);
-            state->image_in_flight.resize(state->swapchain->get_image_count());
-
-            IFenceDesc fence_desc = {};
-            fence_desc.signaled = true;
-
-            ISemaphoreDesc sem_desc = {};
-
-            for (u32 i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
-            {
-                state->available_semaphores[i] = state->device->create_semaphore(sem_desc);
-                state->finished_semaphore[i] = state->device->create_semaphore(sem_desc);
-                state->in_flight_fences[i] = state->device->create_fence(fence_desc);
+                state->frames[i].available_semaphore = state->device->create_semaphore(sem_desc);
+                state->frames[i].finished_semaphore = state->device->create_semaphore(sem_desc);
+                state->frames[i].in_flight_fence = state->device->create_fence(fence_desc);
             }
 
             return state->device != nullptr;
@@ -148,22 +95,57 @@ namespace mag
             (void)dt;
 
             u32& current_frame = state->current_frame;
-            state->in_flight_fences[current_frame]->wait();
+            state->frames[current_frame].in_flight_fence->wait();
 
-            state->swapchain->acquire_next_image(state->available_semaphores[current_frame].get());
+            state->swapchain->acquire_next_image(state->frames[current_frame].available_semaphore.get());
             const u32 image_index = state->swapchain->get_current_image_index();
 
-            if (state->image_in_flight[image_index] != nullptr)
-            {
-                state->image_in_flight[image_index]->wait();
-            }
-            state->image_in_flight[image_index] = state->in_flight_fences[current_frame].get();
+            // Render Passes
+            // -------------------------------------------------------------------------------------------------
+            unique<IRenderPass> render_pass;
+            unique<IRenderingAttachment> color_attachment;
 
-            state->graphics_queue->submit(
-                state->available_semaphores[current_frame].get(), state->finished_semaphore[current_frame].get(),
-                state->in_flight_fences[current_frame].get(), state->command_buffers[image_index].get());
+            IRenderingAttachmentDesc color_attachment_desc = {};
+            color_attachment_desc.type = RenderingAttachmentType::Color;
+            color_attachment_desc.clear_color = {0.4f, 0.6f, 0.8f, 1.0f};
+            color_attachment_desc.texture = state->swapchain->get_texture(image_index);
+            color_attachment = state->device->create_render_attachment(color_attachment_desc);
 
-            state->present_queue->present(state->swapchain.get(), state->finished_semaphore[current_frame].get());
+            IRenderPassDesc render_pass_desc = {};
+            render_pass_desc.extent = state->swapchain->get_extent();
+            render_pass_desc.color_attachments.push_back(color_attachment.get());
+            render_pass = state->device->create_render_pass(render_pass_desc);
+
+            state->frames[current_frame].command_buffer->begin_recording();
+
+            state->frames[current_frame].command_buffer->pipeline_barrier(
+                state->swapchain->get_texture(image_index), TextureLayout::ColorAttachment, AccessMask::None,
+                AccessMask::ColorAttachmentWrite, PipelineStage::TopOfPipe, PipelineStage::ColorAttachmentOutput);
+
+            state->frames[current_frame].command_buffer->set_viewport(state->swapchain->get_extent());
+            state->frames[current_frame].command_buffer->set_scissor(state->swapchain->get_extent());
+
+            state->frames[current_frame].command_buffer->begin_rendering(render_pass.get());
+
+            state->frames[current_frame].command_buffer->bind_pipeline(state->graphics_pipeline.get());
+
+            state->frames[current_frame].command_buffer->draw(3);
+
+            state->frames[current_frame].command_buffer->end_rendering();
+
+            state->frames[current_frame].command_buffer->pipeline_barrier(
+                state->swapchain->get_texture(image_index), TextureLayout::Present, AccessMask::ColorAttachmentWrite,
+                AccessMask::None, PipelineStage::ColorAttachmentOutput, PipelineStage::BottomOfPipe);
+
+            state->frames[current_frame].command_buffer->end_recording();
+
+            state->graphics_queue->submit(state->frames[current_frame].available_semaphore.get(),
+                                          state->frames[current_frame].finished_semaphore.get(),
+                                          state->frames[current_frame].in_flight_fence.get(),
+                                          state->frames[current_frame].command_buffer.get());
+
+            state->present_queue->present(state->swapchain.get(),
+                                          state->frames[current_frame].finished_semaphore.get());
 
             current_frame = (current_frame + 1) % MAX_FRAMES_IN_FLIGHT;
         }
