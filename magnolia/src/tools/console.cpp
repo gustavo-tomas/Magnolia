c7ed89810f33d6260fcc68613ee376476fdf95d2
gustavo-tomas <guga.tomas10@gmail.com>
2026-01-19 19:43:32 -0300

    Adding working console without args

--- magnolia/src/tools/console.cpp
+++ magnolia/src/tools/console.cpp
@@ -10,11 +10,14 @@
 
 #include <imgui/backends/imgui_impl_sdl3.h>
 #include <imgui/backends/imgui_impl_sdlrenderer3.h>
+#include <imgui/misc/cpp/imgui_stdlib.h>
 
 #include <unordered_map>
 
 #include "magnolia/core/assert.hpp"
 #include "magnolia/core/logger.hpp"
+#include "magnolia/core/string.hpp"
+#include "magnolia/math/types.hpp"
 
 // @TODO: we use the SDL renderer instead of vulkan for simplicity. Maybe its a
 // good idea to reuse the window.cpp code to create windows and choose a backend.
@@ -23,22 +26,64 @@
 
 namespace mag
 {
+#define COLOR_WHITE (math::vec4(1.0f, 1.0f, 1.0f, 1.0f))
+#define COLOR_BLUE (math::vec4(0.42f, 0.64f, 0.89f, 1.0f))
+#define COLOR_RED (math::vec4(1.0f, 0.2f, 0.2f, 1.0f))
+#define COLOR_BROWN (math::vec4(1.0f, 0.8f, 0.6f, 1.0f))
+
     namespace console
     {
+        struct LogData
+        {
+                math::vec4 color = COLOR_WHITE;
+                str text = "";
+        };
+
         struct ConsoleState
         {
                 SDL_Window* window = nullptr;
                 SDL_Renderer* renderer = nullptr;
 
                 std::unordered_map<str, std::function<void(const str&)>> commands;
+
+                std::vector<LogData> items;
+                std::vector<str> history;
+                i64 history_pos = -1;  // -1: new line, 0..History.Size-1 browsing history.
+                ImGuiTextFilter filter;
+                const u32 history_display_size = 15;
+                b8 scroll_to_bottom = false;
+                b8 auto_scroll = true;
         };
 
         static ConsoleState* state = nullptr;
+
+        void create_window();
+
+        void destroy_window();
+
+        void initialize_console();
+
+        void draw_console();
+
+        i32 text_edit_callback(ImGuiInputTextCallbackData* data);
+
+        void clear_log();
+
+        template <typename... Args>
+        void add_log(const LogData log, const Args&... args);
 
         b8 initialize()
         {
             state = new ConsoleState();
 
+            create_window();
+            initialize_console();
+
+            return state != nullptr;
+        }
+
+        void create_window()
+        {
             const i32 width = 800;
             const i32 height = 600;
             const SDL_WindowFlags flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN;
@@ -75,11 +120,47 @@ namespace mag
 
             ImGui_ImplSDL3_InitForSDLRenderer(state->window, state->renderer);
             ImGui_ImplSDLRenderer3_Init(state->renderer);
+        }
 
-            return state != nullptr;
+        void initialize_console()
+        {
+            clear_log();
+
+            // Mag commands are blue to distinguish from user commands
+
+            register_command("HELP",
+                             [](const str)
+                             {
+                                 add_log({.color = COLOR_BLUE, .text = "Commands:"});
+                                 for (const auto& [command, function] : state->commands)
+                                 {
+                                     add_log({.color = COLOR_BLUE, .text = "- {0}"}, command.c_str());
+                                 }
+                             });
+
+            register_command("CLEAR", [](const str) { clear_log(); });
+
+            register_command("HISTORY",
+                             [](const str)
+                             {
+                                 const i32 first = state->history.size() - state->history_display_size;
+                                 for (u64 i = first > 0 ? first : 0; i < state->history.size(); i++)
+                                 {
+                                     add_log({.color = COLOR_BLUE, .text = "{0:3}: {1}"}, i, state->history[i].c_str());
+                                 }
+                             });
+
+            add_log({.text = "Command console. Enter 'HELP' for more information."});
         }
 
         void shutdown()
+        {
+            destroy_window();
+
+            delete state;
+        }
+
+        void destroy_window()
         {
             ImGui_ImplSDLRenderer3_Shutdown();
             ImGui_ImplSDL3_Shutdown();
@@ -87,8 +168,6 @@ namespace mag
 
             SDL_DestroyRenderer(state->renderer);
             SDL_DestroyWindow(state->window);
-
-            delete state;
         }
 
         void register_command(const str& command, const std::function<void(const str args)>& func)
@@ -104,14 +183,26 @@ namespace mag
 
         void execute_command(const str& command, const str& args)
         {
+            add_log({.color = COLOR_BROWN, .text = "# {0}"}, command);
+
+            // Insert into state->history
+            state->history_pos = -1;
+            state->history.push_back(command);
+
+            // Process command
             auto it = state->commands.find(command);
-            if (it != state->commands.end())
+            if (it == state->commands.end())
             {
-                it->second(args);
-                return;
+                add_log({.color = COLOR_RED, .text = "Unknown command: '{0}'"}, command);
             }
 
-            LOG_ERROR("Command '{0}' is not registered", command);
+            else
+            {
+                it->second(args);
+            }
+
+            // On command input, we scroll to bottom even if AutoScroll == false
+            state->scroll_to_bottom = true;
         }
 
         void on_update()
@@ -138,7 +229,7 @@ namespace mag
 
             ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport(), dock_flags);
 
-            ImGui::ShowDemoWindow();
+            draw_console();
 
             // Frame end
             ImGui::Render();
@@ -148,6 +239,293 @@ namespace mag
             ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
             SDL_RenderPresent(renderer);
         }
+
+        void draw_console()
+        {
+            if (!ImGui::Begin("Console"))
+            {
+                ImGui::End();
+                return;
+            }
+
+            ImGui::SameLine();
+            ImGui::SetNextItemShortcut(ImGuiMod_Ctrl | ImGuiKey_L, ImGuiInputFlags_Tooltip);
+            if (ImGui::SmallButton("Clear"))
+            {
+                clear_log();
+            }
+
+            ImGui::SameLine();
+            ImGui::SetNextItemShortcut(ImGuiMod_Ctrl | ImGuiKey_C, ImGuiInputFlags_Tooltip);
+            b8 copy_to_clipboard = ImGui::SmallButton("Copy");
+
+            ImGui::Separator();
+
+            // Options menu
+            if (ImGui::BeginPopup("Options"))
+            {
+                ImGui::Checkbox("Auto-scroll", &state->auto_scroll);
+                ImGui::EndPopup();
+            }
+
+            // Options, Filter
+            if (ImGui::Button("Options"))
+            {
+                ImGui::OpenPopup("Options");
+            }
+
+            ImGui::SameLine();
+            state->filter.Draw("Filter (\"incl,-excl\") (\"error\")", 180);
+
+            ImGui::Separator();
+
+            // Reserve enough left-over height for 1 separator + 1 input text
+            const f32 footer_height_to_reserve = ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
+
+            if (ImGui::BeginChild("ScrollingRegion", ImVec2(0, -footer_height_to_reserve), ImGuiChildFlags_NavFlattened,
+                                  ImGuiWindowFlags_HorizontalScrollbar))
+            {
+                if (ImGui::BeginPopupContextWindow())
+                {
+                    if (ImGui::Selectable("Clear"))
+                    {
+                        clear_log();
+                    }
+                    ImGui::EndPopup();
+                }
+
+                ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1));  // Tighten spacing
+                if (copy_to_clipboard)
+                {
+                    ImGui::LogToClipboard();
+                }
+
+                for (const LogData& item : state->items)
+                {
+                    const c8* text = item.text.c_str();
+
+                    if (!state->filter.PassFilter(text))
+                    {
+                        continue;
+                    }
+
+                    const ImVec4 color = ImVec4(item.color.r, item.color.g, item.color.b, item.color.a);
+
+                    ImGui::PushStyleColor(ImGuiCol_Text, color);
+
+                    ImGui::TextUnformatted(text);
+
+                    ImGui::PopStyleColor();
+                }
+
+                if (copy_to_clipboard)
+                {
+                    ImGui::LogFinish();
+                }
+
+                // Keep up at the bottom of the scroll region if we were already at the bottom at the beginning
+                // of the frame. Using a scrollbar or mouse-wheel will take away from the bottom edge.
+                if (state->scroll_to_bottom || (state->auto_scroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY()))
+                {
+                    ImGui::SetScrollHereY(1.0f);
+                }
+
+                state->scroll_to_bottom = false;
+
+                ImGui::PopStyleVar();
+            }
+
+            ImGui::EndChild();
+
+            ImGui::Separator();
+
+            // Command-line
+            b8 reclaim_focus = false;
+
+            const ImGuiInputTextFlags input_text_flags =
+                ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_EscapeClearsAll |
+                ImGuiInputTextFlags_CallbackCompletion | ImGuiInputTextFlags_CallbackHistory;
+
+            str command = "";
+
+            ImGui::PushItemWidth(-1);
+
+            if (ImGui::InputText("##Input", &command, input_text_flags,
+                                 [](ImGuiInputTextCallbackData* data) -> i32 { return text_edit_callback(data); }))
+            {
+                string::trimr(command);
+
+                if (!command.empty())
+                {
+                    // @TODO: take in command args
+                    execute_command(command, "");
+                }
+
+                reclaim_focus = true;
+            }
+
+            ImGui::PopItemWidth();
+
+            // Auto-focus on window apparition
+            ImGui::SetItemDefaultFocus();
+
+            // Auto focus previous widget
+            if (reclaim_focus)
+            {
+                ImGui::SetKeyboardFocusHere(-1);
+            }
+
+            ImGui::End();
+        }
+
+        i32 text_edit_callback(ImGuiInputTextCallbackData* data)
+        {
+            switch (data->EventFlag)
+            {
+                // Text completion
+                case ImGuiInputTextFlags_CallbackCompletion:
+                {
+                    // Locate beginning of current word
+                    const c8* word_end = data->Buf + data->CursorPos;
+                    const c8* word_start = word_end;
+                    while (word_start > data->Buf)
+                    {
+                        const c8 c = word_start[-1];
+                        if (c == ' ' || c == '\t' || c == ',' || c == ';')
+                        {
+                            break;
+                        }
+                        word_start--;
+                    }
+
+                    // Build a list of candidates
+                    std::vector<str> candidates;
+
+                    for (const auto& [command, function] : state->commands)
+                    {
+                        const b8 equal = string::equalni(command, word_start, static_cast<i32>(word_end - word_start));
+
+                        if (equal)
+                        {
+                            candidates.push_back(command);
+                        }
+                    }
+
+                    // No match
+                    if (candidates.empty())
+                    {
+                        add_log({.color = COLOR_RED, .text = "No match for '{0}'!"}, word_start);
+                    }
+
+                    // Single match. Delete the beginning of the word and replace it entirely so we've got nice casing.
+                    else if (candidates.size() == 1)
+                    {
+                        data->DeleteChars(static_cast<i32>(word_start - data->Buf),
+                                          static_cast<i32>(word_end - word_start));
+                        data->InsertChars(data->CursorPos, candidates[0].c_str());
+                        data->InsertChars(data->CursorPos, " ");
+                    }
+
+                    // Multiple matches. Complete as much as we can.
+                    else
+                    {
+                        i32 match_len = static_cast<i32>(word_end - word_start);
+                        b8 all_candidates_matches = true;
+
+                        while (all_candidates_matches)
+                        {
+                            i32 c = 0;
+
+                            for (u64 i = 0; i < candidates.size() && all_candidates_matches; i++)
+                            {
+                                const c8 candidate_c = std::toupper(candidates[i][match_len]);
+                                if (i == 0)
+                                {
+                                    c = candidate_c;
+                                }
+                                else if (c == 0 || c != candidate_c)
+                                {
+                                    all_candidates_matches = false;
+                                }
+                            }
+
+                            if (all_candidates_matches)
+                            {
+                                match_len++;
+                            }
+                        }
+
+                        if (match_len > 0)
+                        {
+                            data->DeleteChars(static_cast<i32>(word_start - data->Buf),
+                                              static_cast<i32>(word_end - word_start));
+                            data->InsertChars(data->CursorPos, candidates[0].c_str(),
+                                              candidates[0].c_str() + match_len);
+                        }
+
+                        // List matches
+                        add_log({.text = "Possible matches:"});
+
+                        for (u64 i = 0; i < candidates.size(); i++)
+                        {
+                            add_log({.text = "- {0}"}, candidates[i]);
+                        }
+                    }
+
+                    break;
+                }
+
+                // History
+                case ImGuiInputTextFlags_CallbackHistory:
+                {
+                    const i32 prev_history_pos = state->history_pos;
+                    if (data->EventKey == ImGuiKey_UpArrow)
+                    {
+                        if (state->history_pos == -1)
+                        {
+                            state->history_pos = state->history.size() - 1;
+                        }
+                        else if (state->history_pos > 0)
+                        {
+                            state->history_pos--;
+                        }
+                    }
+
+                    else if (data->EventKey == ImGuiKey_DownArrow)
+                    {
+                        if (state->history_pos != -1)
+                        {
+                            if (++state->history_pos >= static_cast<i64>(state->history.size()))
+                            {
+                                state->history_pos = -1;
+                            }
+                        }
+                    }
+
+                    // A better implementation would preserve the data on the current input line along with
+                    // cursor position.
+                    if (prev_history_pos != state->history_pos)
+                    {
+                        const str history_str = (state->history_pos >= 0) ? state->history[state->history_pos] : "";
+
+                        data->DeleteChars(0, data->BufTextLen);
+                        data->InsertChars(0, history_str.c_str());
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        template <typename... Args>
+        void add_log(const LogData log, const Args&... args)
+        {
+            const str formatted_text = log::get_formatted_log(log.text, args...);
+
+            state->items.push_back({.color = log.color, .text = formatted_text});
+        }
+
+        void clear_log() { state->items.clear(); }
 
         void on_event(const void* event)
         {
